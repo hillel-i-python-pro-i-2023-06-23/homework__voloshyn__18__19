bad23489183f171f28a3235a519ccb93a1f9f434
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2023-10-27 18:52:44 +0300

    first

--- application/models.py
+++ application/models.py
@@ -1,23 +1,10 @@
 from django.db import models
 
-
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-
-
-class ContactData(models.Model):
-    contact = models.ForeignKey(Contact, related_name="contact_data", on_delete=models.CASCADE)
-    data = models.CharField(max_length=100)
-    DATA_TYPES = (
-        ("phone", "Phone"),
-        ("email", "Email"),
-    )
-    data_type = models.CharField(max_length=10, choices=DATA_TYPES, default="phone")
-
-    def __str__(self):
-        return self.data
